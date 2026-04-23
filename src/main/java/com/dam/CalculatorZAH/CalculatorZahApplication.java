27c82f6a710795358f3da1f9a9241b5348e1e840
HugoZap11 <hugozap11@gmail.com>
2026-04-23 20:12:29 +0200

    Creacion del Calculator

---
+++ src/main/java/com/dam/CalculatorZAH/CalculatorZahApplication.java
@@ -0,0 +1,13 @@
+package com.dam.CalculatorZAH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorZahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorZahApplication.class, args);
+	}
+
+}
